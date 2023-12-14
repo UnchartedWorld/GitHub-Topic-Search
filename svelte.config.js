1922e59e89d80534a65275ee26de527a8fa3540f
UnchartedWorld <nathangvjcollins@gmail.com>
2023-12-14 16:40:13 +0000

    fix?(config): Explicitly defines repo name
    This (may) fix the routing issue on GitHub, but likely not. I'll learn quickly based on a failed build email

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 			fallback: '404.html',
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/GitHub-Topic-Search' : '',
+			base: process.argv.includes('dev') ? '' : "/GitHub-Topic-Search"
 		}
 	}
 };
