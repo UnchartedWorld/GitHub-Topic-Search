f3e487e4a3a537b26ed0fc3b4cf5041e90e91a72
UnchartedWorld <nathangvjcollins@gmail.com>
2024-01-04 01:43:17 +0000

    feat(components): Updates Card to look more aesthetically pleasing
    This is still in progress as I need a fork SVG, alongside refinement of the card, but the core concept is good and acceptable.

--- src/components/GitHubCard.svelte
+++ src/components/GitHubCard.svelte
@@ -1,14 +1,35 @@
 <script lang="ts">
-    export let cardTitle: string;
-    export let cardDescription: string;
-    export let cardFooterText: string;
+	export let cardID: number;
+	export let cardUserAvatar: string;
+	export let cardHTML: string;
+	export let cardTitle: string;
+	export let cardCreatorName: string;
+	export let cardDescription: string;
+	export let starCount: number;
+	export let forkCount: number;
 </script>
 
-<section class="border shadow-md rounded-md max-w-md bg-slate-300 dark:bg-slate-800">
-    <div class="p-4 md:p-5">
-        <h3>{cardTitle}</h3>
-        <p class="mt-1 text-gray-500 dark:text-gray-400">{cardDescription}</p>
-        <p class="mt-5 text-xs text-gray-600 dark:text-gray-600">{cardFooterText}</p>
-    </div>
-
-</section>
+<a href={cardHTML}>
+	<section
+		id={cardID.toString()}
+		class="border h-full rounded-md max-w-md
+           bg-white dark:bg-slate-800 shadow-md hover:shadow-lg transition duration-300 p-5"
+	>
+		<div class="p-4 md:p-5 flex flex-col h-full">
+			<h3 class="text-xl font-bold break-words">{cardTitle}</h3>
+			<p class="text-neutral-700 md:line-clamp-2">{cardDescription}</p>
+			<div class="flex flex-row">
+				<img class="w-5 h-5 mr-1" src={cardUserAvatar} alt="GitHub account avatar" />
+				<p>Creator: {cardCreatorName}</p>
+			</div>
+			
+			<div class="flex flex-grow justify-between items-end">
+				<div class="flex flex-row">
+					<img class="w-5 h-5 mr-1" src="/star.svg" alt="GitHub stars" />
+					<p>{starCount}</p>
+				</div>
+				<p>Fork: {forkCount}</p>
+			</div>
+		</div>
+	</section>
+</a>
