a638202e3e88b31d0ba46c94180378e24b5991b5
UnchartedWorld <nathangvjcollins@gmail.com>
2024-01-06 23:48:16 +0000

    feat(components): Updates header to have dark variant + slight padding

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,10 +2,10 @@
 	import { base } from "$app/paths";
 </script>
 
-<header class="bg-slate-50 dark:bg-slate-800 drop-shadow-md">
+<header class="bg-neutral-100 dark:bg-neutral-800 drop-shadow-md">
     <nav class="px-4 lg:px-6 py-3">     
         <div class="flex flex-wrap justify-between items-center mx-auto">
-            <ul class="flex flex-row font-medium space-x-8 mt-0">
+            <ul class="flex flex-row font-medium space-x-8 mt-0 px-6 dark:text-gray-100">
                 <li>
                     <a href="{base}/">Home</a>
                 </li>
