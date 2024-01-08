e2bebaef392a098e811e899addcb57b2e05e2bc7
UnchartedWorld <nathangvjcollins@gmail.com>
2024-01-08 22:23:56 +0000

    feat(components): Creates gradient loading spinner for use elsewhere
    This is naturally rather large, and is heavily inspired by online guides, but for the purposes of this website it'll do absolutely fine.

---
+++ src/components/LoadingSpinner.svelte
@@ -0,0 +1,8 @@
+<div class="flex items-center justify-center py-20">
+	<div
+		class="flex h-20 w-20 animate-spin rounded-full items-center justify-center
+           bg-gradient-to-tr from-blue-400 to-blue-700 dark:from-blue-700 dark:to-blue-400"
+	>
+        <div class="h-14 w-14 rounded-full bg-neutral-100 dark:bg-neutral-800"></div>
+    </div>
+</div>
