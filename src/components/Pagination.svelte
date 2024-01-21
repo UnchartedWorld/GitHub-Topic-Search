41ad45d8285eca38cd4f1629e3a8a0d8b068f66f
UnchartedWorld <nathangvjcollins@gmail.com>
2024-01-21 16:01:10 +0000

    feat(components): Adds hover drop-shadow, responsive text size and increased opacity

--- src/components/Pagination.svelte
+++ src/components/Pagination.svelte
@@ -20,20 +20,22 @@
 		<li>
 			<button
 				class="h-10 px-5 bg-violet-600 hover:bg-violet-800 text-white dark:text-neutral-100 font-bold
-            py-2 rounded-full disabled:opacity-60 disabled:cursor-not-allowed"
+            		   py-2 rounded-full disabled:opacity-80 disabled:cursor-not-allowed text-sm sm:text-base shadow-sm
+					   hover:shadow-md"
 				on:click={previousPage}
 				disabled={currentPage === 1}>« Previous</button
 			>
 		</li>
 		<li>
-			<p class="px-2 py-2 dark:text-gray-100">
+			<p class="px-3 py-3 dark:text-gray-100 text-xs sm:text-base">
 				Page <b>{currentPage.toString()}</b> of <b>{totalPages.toString()}</b>
 			</p>
 		</li>
 		<li>
 			<button
 				class="h-10 px-5 bg-violet-600 hover:bg-violet-800 text-white dark:text-neutral-100 font-bold
-            py-2 rounded-full disabled:opacity-60 disabled:cursor-not-allowed"
+            		   py-2 rounded-full disabled:opacity-80 disabled:cursor-not-allowed text-sm sm:text-base shadow-sm
+					   hover:shadow-md"
 				on:click={nextPage}
 				disabled={currentPage === totalPages}>Next »</button
 			>
