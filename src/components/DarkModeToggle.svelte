9782e103d5f1d38e122454a5b739c2b68846ee85
UnchartedWorld <nathangvjcollins@gmail.com>
2024-01-22 22:02:36 +0000

    feat(components): Updates dark mode button to have outline & better colouration

--- src/components/DarkModeToggle.svelte
+++ src/components/DarkModeToggle.svelte
@@ -12,7 +12,8 @@
 </script>
 
 <button
-	class="px-2 py-1 mr-3 shadow-md rounded-md bg-violet-100 dark:bg-neutral-700"
+	class="px-2 py-1 mr-3 shadow-md rounded-md bg-violet-50 hover:bg-violet-100 dark:bg-neutral-600
+		  dark:hover:bg-neutral-700 border border-gray-300"
 	on:click={toggleDarkMode}
 >
 	{currentMode}
