3d2d014b1f9c11257b289c673c6ffb73429e9d47
UnchartedWorld <nathangvjcollins@gmail.com>
2024-01-22 22:04:13 +0000

    feat(components): Changes disabled button colour, slightly reduces y-axis padding on middle text

--- src/components/Pagination.svelte
+++ src/components/Pagination.svelte
@@ -20,22 +20,22 @@
 		<li>
 			<button
 				class="h-10 px-5 bg-violet-600 hover:bg-violet-800 text-white dark:text-neutral-100 font-bold
-            		   py-2 rounded-full disabled:opacity-80 disabled:cursor-not-allowed text-sm sm:text-base shadow-sm
-					   hover:shadow-md"
+            		   py-2 rounded-full disabled:opacity-80 disabled:bg-gray-600 disabled:cursor-not-allowed
+					   text-sm sm:text-base shadow-sm hover:shadow-md"
 				on:click={previousPage}
 				disabled={currentPage === 1}>« Previous</button
 			>
 		</li>
 		<li>
-			<p class="px-3 py-3 dark:text-gray-100 text-xs sm:text-base">
+			<p class="px-3 py-2 dark:text-gray-100 text-xs sm:text-base">
 				Page <b>{currentPage.toString()}</b> of <b>{totalPages.toString()}</b>
 			</p>
 		</li>
 		<li>
 			<button
 				class="h-10 px-5 bg-violet-600 hover:bg-violet-800 text-white dark:text-neutral-100 font-bold
-            		   py-2 rounded-full disabled:opacity-80 disabled:cursor-not-allowed text-sm sm:text-base shadow-sm
-					   hover:shadow-md"
+            		   py-2 rounded-full disabled:opacity-80 disabled:bg-gray-600 disabled:cursor-not-allowed
+					   text-sm sm:text-base shadow-sm hover:shadow-md"
 				on:click={nextPage}
 				disabled={currentPage === totalPages}>Next »</button
 			>
